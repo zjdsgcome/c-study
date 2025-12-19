068abd3a0e98af11621cb58d77f3a1696cad0616
jianzhang <2966534592@qq.com>
2025-12-19 17:05:34 +0800

    结构体初探

--- malloc与free.c
+++ malloc与free.c
@@ -3,7 +3,14 @@
 #include<string.h>
 #include<stdlib.h>
 
+typedef struct Person {
+	char name[200];
 
+	unsigned int age;
+
+} Person;
+
+Person* get_person(void);
 
 int main(void) {
 
@@ -22,9 +29,36 @@ int main(void) {
 
 	for (size_t i = 0; i < 10; i++)
 	{
-		printf("p i is %d", *(p+i));//不能使用*p然后p++，因为会修改掉p的指向，后面的free必须需要初始的p
+		printf("p i is %d \n", *(p+i));//不能使用*p然后p++，因为会修改掉p的指向，后面的free必须需要初始的p
 	}
 	free(p);//free后的地址一般来讲不会立即消失，需要手动置为NULL
 
 	p = NULL;
+
+	//结构体
+
+	Person* person = get_person();
+
+	if (person != NULL) {
+		printf("结构体person age is %u \n", (*person).age);//括号不能少,.运算符优先级很高
+		printf("结构体person age is %u \n", person->age);//指针取值的另一种写法
+	}
+
+	free(person);
+
+	person = NULL;//释放指针后，置为NULL，防止野指针
+
+	
+}
+
+Person* get_person(void) {
+	Person* heap_person = (Person*)malloc(sizeof(struct Person));//堆上申请内存，在方法结束后依然存在
+
+	Person stack_person = {"hello world",20};//栈上申请内存，方法结束后就释放了
+
+	if (heap_person != NULL) {
+		(*heap_person).age = stack_person.age;//括号不能少,.运算符优先级很高
+	}
+
+	return heap_person;
 }
